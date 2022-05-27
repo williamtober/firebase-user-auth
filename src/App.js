c66afb049bf9d0507bc40312fe0a524d89faa5b7
William Tober <wvtober@student.fullsail.edu>
2022-05-27 16:05:03 -0400

    store and first slice /User/ slice created, defined dispatch based on expected userAuth data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useInsertionEffect } from 'react'
+import { useDispatch, userDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
+import { auth, onAuthStateChanged } from './firebase'
 
 function App() {
+  const user = userSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useInsertionEffect(() => {
+    if(userAuth) {
+      // if user is logged in lets send that data to redux, that will store the user data in state
+      // lets store some basic information for now
+      dispatch(
+        login({
+          email : userAuth.email,
+          uid : userAuth.uid,
+          displayName : userAuth.displayName,
+          photoUrl : userAuth.photoURL
+        })
+      )
+    } else {
+      dispatch(logout())
+    }
+  }) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Header Section goes here */}
+
+      {/* is user logged in? */}
+      {!user ? (
+        <Login />
+      ) : (
+        // display the app only to authorized users
+        <div className=''>
+          {/*  rest of the app would go here */}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/features/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// redux slices are supposed to be seperate, and pertain to one category of data i.e. user : { get, post, delete, put regarding user data}
+export const userSlice = createSlice({
+    name : 'user',
+    initialState : {
+        user : null,
+    },
+    reducers : {
+        // set state to current payload
+        login : (state, action) => {
+            state.user = action.payload
+        },
+        // clear out the user state
+        logout : (state) => {
+            state.user = null
+        }
+    },
+})
+
+export const { login, logout } = userSlice.actions
+
+// selectors
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducers;
+
+
