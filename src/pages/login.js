acf07d81c446716b528fc731b4d1a6ef5379bd20
William Tober <wvtober@student.fullsail.edu>
2022-06-10 00:36:52 -0400

    light styling, need to move on to load user information, and enable CRUD account operations.

--- src/pages/login.js
+++ src/pages/login.js
@@ -15,6 +15,8 @@ export const Login = () => {
     const [password, setPassword] = useState('')
     const [name, setName] = useState('')
     const [profilePic, setProfilePic] = useState('')
+    // toggles if user wants to register
+    const [newUser, setNewUser] = useState(false)
     const dispatch = useDispatch()
 
 
@@ -62,53 +64,63 @@ export const Login = () => {
         })
     }
 
+    const registerStyle = !newUser ? {display: 'none'} : {display: 'flex'}
+
     return (
-        <div className='flex flex-col items-center justify-center w-[100vw] h-[100vh]'>
-            <form className='rounded-xl w-[400px] h-[600px] flex flex-col items-start justify-center gap-y-8 p-10 bg-green-400 shadow-2xl'>
+        <div className='flex flex-col items-center justify-center w-[100vw] h-[100vh] bg-blue-300'>
+            
+            <form className='rounded-xl w-[400px] h-[600px] flex flex-col items-start justify-center gap-y-8 p-10 bg-[#f4f5f7] shadow border-0'>
+                <h2 className='w-full text-center mb-10 text-2xl font-bold text-gray-500'>User {!newUser ? <span>Login</span> : <span>Registration</span>}</h2>
                 <div className='w-full flex flex-row align-center justify-between'>
-                    <label className='font-bold text-black h-full flex items-center justify-start'>Email</label>
+                    <label className='font-bold text-black h-full hidden items-center justify-start'>Email</label>
                     <input
                         required 
-                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow'
+                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow w-full'
+                        placeholder='Email'
                         type='text'
                         value={email}
                         onChange={e => setEmail(e.target.value)}
                     />
                 </div>
                 <div className='w-full flex flex-row align-center justify-between'>
-                    <label className='font-bold text-black h-full flex items-center justify-start'>Password</label>
+                    <label className='font-bold text-black h-full hidden items-center justify-start'>Password</label>
                     <input 
-                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow'
+                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow w-full'
+                        placeholder='Password'
                         type='text'
                         value={password}
                         onChange={e => setPassword(e.target.value)} 
                     />
                 </div>
-                <div className='w-full flex flex-row align-center justify-between'>
-                    <label className='font-bold text-black h-full flex items-center justify-start'>Name</label>
+                <div style={registerStyle} className='w-full flex flex-row align-center justify-between'>
+                    <label className='font-bold text-black h-full hidden items-center justify-start'>Name</label>
                     <input 
-                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow'
+                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow w-full'
+                        placeholder='Full Name'
                         type='text'
                         value={name}
                         onChange={e => setName(e.target.value)}
                     />
                 </div>
-                <div className='w-full flex flex-row align-center justify-between'>
-                    <label className='font-bold text-black h-full flex items-center justify-start'>Profile Picture</label>
+                <div style={registerStyle} className="w-full flex-row align-center justify-between">
+                    <label className='font-bold text-black h-full hidden items-center justify-start'>Profile Picture</label>
                     <input 
-                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow'
+                        className='rounded py-1 flex items-center justify-start pl-2 focus:shadow w-full'
+                        placeholder='URL to Profile Picture'
                         type='text'
                         value={profilePic}
                         onChange={e => setProfilePic(e.target.value)}
                     />
                 </div>
-                <div className='w-full mt-12 flex flex-col justify-center align-center'>
-                    <input onClick={loginToApp} className='bg-white text-black px-full py-2 rounded' type='button' value='Sign In'>
+                <div className='w-full mt-5 flex flex-col justify-center align-center'>
+                    <input style={newUser ? {display: 'none'} : {display: 'flex'}} onClick={loginToApp} className='bg-blue-500 text-gray-50 font-semibold px-full py-2 rounded justify-center' type='button' value='Sign In'>
                     </input>
                     {/* use hooks to hide other values, until user clicks on register. */}
-                    <span className='w-full mt-8 mb-2 text-center'>Not a User?</span>
-                    <input onClick={register} className='bg-white text-black px-full py-2 rounded' type='button' value='Register'>
+                    <span style={newUser ? {display: 'none'} : {display: 'flex'}} className='w-full mt-2 mb-2 text-center justify-center'>Not a User? <span className='text-blue-400 ml-1' onClick={e => setNewUser(!newUser)}>Register Today. </span></span>
+                    <input style={registerStyle} onClick={register} className='bg-blue-500 text-gray-50 font-semibold py-2 rounded justify-center' type='button' value='Register'>
+                        
                     </input>
+                    <span style={!newUser ? {display: 'none'} : {display: 'flex'}} className='w-full mt-2 mb-2 text-center justify-center'>Already Registered? <span className='text-blue-400 ml-1' onClick={e => setNewUser(!newUser)}>Login Here. </span></span>
                 </div>
                 
             </form>
